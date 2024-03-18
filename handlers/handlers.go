190a17cfbc510793ec8087d6ed32c7f0708e757f
mohiuddinmh <mohiuddinmh@gmail.com>
2024-03-18 18:00:17 -0500

    done deal

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -4,12 +4,13 @@ import (
 	"encoding/json"
 	"net/http"
 
-	"lets-go/database" // Replace with the path to database.go
-	"lets-go/models"   // Replace with the path to models.go
+	"lets-go/database"
+	"lets-go/repositories"
 )
 
 func GetUsers(w http.ResponseWriter, r *http.Request) {
-	users, err := fetchUsersFromDB()
+	userRepository := repositories.NewUserRepository(database.Db)
+	users, err := userRepository.GetUsers()
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
@@ -21,27 +22,4 @@ func GetUsers(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-}
-
-func fetchUsersFromDB() ([]models.User, error) {
-	rows, err := database.Db.Query("SELECT id, last_name, first_name, dob FROM users;")
-	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
-
-	var users []models.User
-	for rows.Next() {
-		var user models.User
-		if err := rows.Scan(&user.ID, &user.LastName, &user.FirstName, &user.DoB); err != nil {
-			return nil, err
-		}
-		users = append(users, user)
-	}
-
-	if err := rows.Err(); err != nil {
-		return nil, err
-	}
-
-	return users, nil
 }
