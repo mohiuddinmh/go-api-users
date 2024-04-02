eef9d2fe7b34633a42472c26b495eb29c6247c80
mohiuddinmh <mohiuddinmh@gmail.com>
2024-04-01 19:13:28 -0500

    minor fix

--- repositories/User_Repository_Impl.go
+++ repositories/User_Repository_Impl.go
@@ -14,16 +14,18 @@ func NewUserRepository(db *sql.DB) UserRepository {
 }
 
 func (r *UserRepositoryImpl) GetUsers() ([]models.User, error) {
-	rows, err := r.db.Query("SELECT id, last_name, first_name, dob FROM users;")
+	rows, err := r.db.Query("SELECT id, first_name, last_name, dob FROM users;")
+
 	if err != nil {
 		return nil, err
 	}
+
 	defer rows.Close()
 
 	var users []models.User
 	for rows.Next() {
 		var user models.User
-		if err := rows.Scan(&user.ID, &user.LastName, &user.FirstName, &user.DoB); err != nil {
+		if err := rows.Scan(&user.ID, &user.FirstName, &user.LastName, &user.DoB); err != nil {
 			return nil, err
 		}
 		users = append(users, user)
